61c003f49e51676596dc729bc0669886df2502d1
James Peavy Jr <jamespeavyjr@Jamess-MacBook-Air.local>
2023-07-01 23:15:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {useForm} from "react-hook-form"
+import './App.css';
+
+function App(){
+  const {register, handleSubmit, formState : {errors},}=useForm();
+
+
+
+
+const onSubmit=(data)=>{
+  console.log(data);
+}
+
+return(
+  <div>
+    <h1>React Hook Form</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" id="name" {...register("name", {required: true})} placeholder="Username" />
+      {errors.name && <span>Username is required</span>}
+      <br/>
+      <input type="email" id="email" {...register("email", {required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, } )}placeholder="Email"/>
+      {errors.email && <span>Please enter a valid email address</span>}
+      <br/>
+      <input type="password" id="password" {...register("password", {required: true})} placeholder="Password"/>
+      {errors.password && <span>Password is required</span>}
+      <br/>
+      <button type="submit">Submit</button>   
+    </form>
+  </div>
+)
+}
+
+export default App;
